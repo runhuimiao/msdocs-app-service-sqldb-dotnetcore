9a72005309e01e08a6b2347ff74237ff728ed72b
runhuimiao <68601736+runhuimiao@users.noreply.github.com>
2024-02-02 22:29:37 +0800

    test1

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+
 builder.Services.AddStackExchangeRedisCache(options =>
 {
 options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
